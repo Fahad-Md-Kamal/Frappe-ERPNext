d9fbc3ca90c429c5134b5242d0e10b5d560c1cda
fahad-md-kamal <faahad.hossain@gmail.com>
2021-04-20 13:18:55 +0600

    add & modify custom btn & chng field value

--- estate_app/estate_app/doctype/property/property.js
+++ estate_app/estate_app/doctype/property/property.js
@@ -2,7 +2,26 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Property', {
-	// refresh: function(frm) {
+	setup: function(frm){
+		console.log(frm);
+		console.log(frm.selected_doc['name']);
+		console.log(frm.selected_doc.amenities);
+	},
 
-	// }
+	refresh: function(frm) {
+
+		let btn = frm.add_custom_button('Extra Button', () => {
+			frappe.prompt('Address', ({ value }) => {
+				if (value){
+					frm.set_value('address', value);
+					frm.refresh_field('address');
+					frappe.show_alert(`Address field updated with the value ${value}`, 5);
+				}
+			})
+		});
+
+		// Change Color of the Button
+		btn[0].classList.replace('btn-default','btn-info');
+	},
+	
 });
